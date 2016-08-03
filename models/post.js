00e9d141b80a97f1a9877d6370f9618f6581212d
Andrew <andrewday40@gmail.com>
2016-08-02 20:59:46 -0400

    created post model

---
+++ models/post.js
@@ -0,0 +1,31 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  body: {
+    type: String,
+    require: true
+  },
+  created: {
+    type: Date,
+    require: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true
+  },
+  update: {
+    type: Date,
+    require: true
+  }
+});
+
+
+var Post = mongoose.model('Post', postSchema);
+module.export = Post;
