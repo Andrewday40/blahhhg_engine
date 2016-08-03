c8cef9a92286fed0f473fba9f4690d14dc059e06
Andrew <andrewday40@gmail.com>
2016-08-03 19:48:54 -0400

    created basic post routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,31 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('')
+router.get('/posts/:id', getPost);
+router.get('/posts', getAllThePost);
+router.post('/posts', createPost);
+router.delete('/posts/:id', deletePost);
+router.put('/posts/:id', updatePost);
+
+module.exports = router;
+
+function getPost(req, res, next){
+  console.log('getting a post');
+  next();
+}
+function getAllThePost(req, res, next){
+  console.log('getting all the posts');
+  next();
+}
+function createPost(req, res, next){
+  console.log('creating a post');
+  next();
+}
+function deletePost(req, res, next){
+  console.log('deleteing a post');
+  next();
+}
+function updatePost(req, res, next){
+  console.log('updating a post');
+  next();
+}
