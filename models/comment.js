ec94d07ccec1527fb68f673b147f29d89099aa2b
Andrew <andrewday40@gmail.com>
2016-08-05 18:36:48 -0400

    added update comment route

--- models/comment.js
+++ models/comment.js
@@ -29,5 +29,9 @@ var commentSchema = new Schema({
   // }
 });
 
+commentSchema.pre('findOneAndUpdate', function(){
+  this.update({},{ $set: { updated: new Date() } });
+});
+
 var Comment = mongoose.model('Comment', commentSchema);
 module.exports = Comment;
