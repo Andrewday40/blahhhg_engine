ce279360f8005d34d7dc11556536637130e4467c
Andrew <andrewday40@gmail.com>
2016-08-16 19:02:26 -0400

    added password salt and hash

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var userSchema = new Schema({
   firstName: {
@@ -19,8 +20,30 @@ var userSchema = new Schema({
     type: String,
     required: true,
     unique: true
+  },
+  hash: {
+    type: String,
+    required: false
+  },
+  salt: {
+    type: String,
+    required: false
   }
 });
+
+userSchema.method.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                    .toString('hex');
+};
+
+userSchema.method.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+                   .toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.method.generateJwt = function(){};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
