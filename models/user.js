5d8749b3efc66d948224d23438b53d24aef03f8a
Andrew <andrewday40@gmail.com>
2016-08-16 19:17:17 -0400

    fixed typos in user.js

--- models/user.js
+++ models/user.js
@@ -31,19 +31,19 @@ var userSchema = new Schema({
   }
 });
 
-userSchema.method.setPassword = function(password){
+userSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
                     .toString('hex');
 };
 
-userSchema.method.validPassword = function(password){
+userSchema.methods.validPassword = function(password){
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
                    .toString('hex');
   return this.hash === hash;
 };
 
-userSchema.method.generateJwt = function(){};
+userSchema.methods.generateJwt = function(){};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
