fedd8651d7c384010e33422008341c28e2d2379b
Andrew <andrewday40@gmail.com>
2016-08-17 20:41:09 -0400

    finished login route

--- authentication.js
+++ authentication.js
@@ -3,7 +3,7 @@ var passport = require('passport');
 var mongoose = require('mongoose');
 var User = require('./models/user.js');
 
-module.exporst = {
+module.exports = {
   signup: signup,
   login: login
 };
@@ -41,5 +41,5 @@ function login(req, res){
     } else {
       return res.status(401).json(info);
     }
-  });
+  })(req, res);
 }
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,15 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
+var auth = require('../authentication.js');
+var passport = require('passport');
 
 
 router.get('/users', getAllUser);
 router.get('/users/:userId', getUser);
-router.post('/users', createUser);
+// router.post('/users', createUser);
+router.post('/signup', auth.signup);
+router.post('/login', auth.login);
 router.delete('/users/:userId', deleteUser);
 router.put('/users/:userId', updateUser);
 
